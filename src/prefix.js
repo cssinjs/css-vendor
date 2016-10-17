0a3653217056fc032b5e85ad9393bc77a44056e4
Abel Toledano <atabel87@gmail.com>
2016-10-17 18:49:07 +0200

    Use a simple foor loop instead of .find to avoid using a polyfill

--- src/prefix.js
+++ src/prefix.js
@@ -17,15 +17,21 @@ if (isBrowser) {
     ms: '-ms-',
     O: '-o-'
   }
-  const prefixes = ['Moz', 'Webkit', 'ms', 'O']
+
+  // Order matters. We need to check Webkit the last one because
+  // other vendors use to add Webkit prefixes to some properties
+  const prefixes = ['Moz', 'ms', 'O', 'Webkit']
 
   const style = document.createElement('p').style
   const testProp = 'Transform'
 
-  const vendor = prefixes.find(prefix => (prefix + testProp) in style)
-  if (vendor) {
-    js = vendor
-    css = jsCssMap[vendor]
+  for (let i = 0; i < prefixes.length; i++) {
+    const prefix = prefixes[i];
+    if ((prefix + testProp) in style) {
+      js = prefix
+      css = jsCssMap[prefix]
+      break
+    }
   }
 }
 
