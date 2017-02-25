b286c5910fb9f251dc8b46fc7e8785d48be1e8d4
Chi Vinh Le <vinh@wikiwi.io>
2017-02-25 23:49:59 +0700

    Lint files in test folder

--- test/fixtures/property-prefix.js
+++ test/fixtures/property-prefix.js
@@ -10,29 +10,29 @@ const prefixer = postcssJs.sync([ap])
 
 const skipProperties = [
   // caniuse doesn't cover this property and spec might drop this: https://www.w3.org/TR/css-fonts-3/.
-  "font-language-override",
+  'font-language-override',
 ]
 
 const isNotSupported = (o) =>
-    o.level === "none" ||
+    o.level === 'none' ||
     // http://caniuse.com/#feat=object-fit
-    o.property === "object-position" && o.notes.indexOf(1) > -1 ||
+    o.property === 'object-position' && o.notes.indexOf(1) > -1 ||
     // http://caniuse.com/#feat=multicolumn
     // https://bugzilla.mozilla.org/show_bug.cgi?id=616436
-    o.property === "column-span" && currentBrowser.id === "firefox" ||
+    o.property === 'column-span' && currentBrowser.id === 'firefox' ||
     // http://caniuse.com/#feat=css-masks
     o.property.match(/^mask-/) && o.notes.indexOf(2) > -1
 
 function generateFixture() {
   const fixture = {}
-  const supported = Object.keys(data).
+  Object.keys(data).
     // Filters autoprefixer data to include only property prefix related entries.
     filter(s => s.match(/^[^:@].*$/g)).
     filter(s => data[s].props === undefined).
     filter(s => skipProperties.indexOf(s) < 0).
     // TODO: Remove the following line when this is resolved: https://github.com/Fyrd/caniuse/issues/3070.
-    filter(s => ["css3-cursors-grab", "css-text-spacing"].indexOf(data[s].feature) < 0).
-    map(s => ({ property: s, feature: data[s].feature, ...getSupport(data[s].feature) })).
+    filter(s => ['css3-cursors-grab', 'css-text-spacing'].indexOf(data[s].feature) < 0).
+    map(s => ({property: s, feature: data[s].feature, ...getSupport(data[s].feature)})).
     // No support in caniuse db means no support for the spec, but
     // no support in css-vendor means no browser support at all for the particular property,
     // therefore we cannot test with the caniuse data for these cases.
