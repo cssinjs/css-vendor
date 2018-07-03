4b9fd35ce5b47817d5a5b4bd8c4dc34d5e2aae66
Oleh Aloshkin <olegaleshkin@gmail.com>
2018-07-04 02:19:03 +0300

    Fixed build for webpack@4

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,9 @@
-const webpack = require('webpack')
-
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  })
-]
-
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin())
-}
-
 module.exports = {
   entry: ['./src/index.js'],
   output: {
     library: 'cssVendor',
     libraryTarget: 'umd'
   },
-  plugins,
   module: {
     rules: [{
       loader: 'babel-loader',
