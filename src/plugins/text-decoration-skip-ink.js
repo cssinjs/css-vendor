50abfefc37a06f9f35297676cee83ea312aeb635
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-02-06 00:36:59 +0100

    Added new plugin for text-decoration-skip-ink property

---
+++ src/plugins/text-decoration-skip-ink.js
@@ -0,0 +1,12 @@
+import prefix from '../prefix'
+
+// Support old appearance props syntax.
+// https://caniuse.com/#feat=text-decoration
+export default {
+  noPrefill: ['text-decoration-skip-ink'],
+  supportedProperty: (prop) => {
+    if (prop !== 'text-decoration-skip-ink') return false
+    if (prefix.js === 'Webkit') return `-webkit-${prop}`
+    return prefix.css + prop
+  }
+}
