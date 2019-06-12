953972ef1f51ecb14e52e012b9492293617c56f3
Oleh Aloshkin <olegaleshkin@gmail.com>
2019-06-12 16:33:02 +0200

    Cover case, where prefix goes on seconds position in string

--- src/plugins/prefixed.js
+++ src/plugins/prefixed.js
@@ -5,8 +5,10 @@ import pascalize from '../utils/pascalize'
 export default {
   supportedProperty: (prop, style) => {
     const pascalized = pascalize(prop)
-    // Return custom CSS variable or already prefixed value without prefixing.
-    if (prop[0] === '-' || prop[1] === '-') return prop
+    // Return custom CSS variable without prefixing.
+    if (prop[0] === '-') return prop
+    // Return already prefixed value without prefixing.
+    if (prop[0] === '-' && prop[1] === '-') return prop
     if (prefix.js + pascalized in style) return prefix.css + prop
     // Try webkit fallback.
     if (prefix.js !== 'Webkit' && `Webkit${pascalized}` in style) return `-webkit-${prop}`
